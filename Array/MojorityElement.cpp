1d9a6bf4e0b3c11446be4d0c0088f8d98f2982c5
Sohag Abir <sohagabir112@gmail.com>
2025-12-25 22:23:37 +0600

    Add files via upload

---
+++ Array/MojorityElement.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+majorityElement(int arr[]){
+    int n = arr[]
+}
+void printArray(int arr[], int n){
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int n;
+    cout<<"Enter Array size: ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+
+    cout<<"Unsorted Array: ";
+    printArray(arr,n);
+
+    majorityElement(arr);
+    
+
+    return 0;
+}
